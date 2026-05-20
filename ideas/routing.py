1fa484a97f0eca7ffc9ec76a35b785ddd2b64df5
sibclean <33279665+sibclean@users.noreply.github.com>
2026-05-20 19:40:50 +0600

    Update routing.py

--- ideas/routing.py
+++ ideas/routing.py
@@ -1,7 +1,6 @@
 from django.urls import re_path
 from . import consumers
 
-# Здесь мы прописываем URL, по которому браузер будет подключаться к сокету
 websocket_urlpatterns = [
     re_path(r'ws/chat/(?P<conversation_id>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
